ab227cf826bf03728114de6b8f8f151d727c689c
Jaidev sah <jaidevsah007@gmail.com>
2023-01-07 07:54:30 +0530

    Initial Commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,20 @@ import '../styles/App.css';
 const App = () => {
 
   const handleDoubleClick = (event) =>{
-  
+   if(event.target.id === "dblclick-btn"){
+    console.log("I was double clicked")
+   }
+  }
+  const handleClick = (event) => {
+   if(event.target.type === "submit"){
+    console.log("I was not double clicked")
+   }
   }
   return (
     <div id="main">
-      <button id="dblclick-btn">Double click me</button>
+      <button id="dblclick-btn" onClick={handleClick} onDoubleClick={handleDoubleClick}>Double click me</button>
     </div>
   )
 }
-
 
 export default App;
